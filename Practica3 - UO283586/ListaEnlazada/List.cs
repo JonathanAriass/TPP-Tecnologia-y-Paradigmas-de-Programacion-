153a7d158ca8e39478ffe443d6435191c7aedd16
Jonathan Arias Busto <94826968+JonathanAriass@users.noreply.github.com>
2022-02-16 12:53:36 +0100

    Practica 3 (Unfinished)
    Left to do:
    - Comment the code
    - Implement the polymorphic list contract programmation
    - Do test for the contract programmation of list

---
+++ Practica3 - UO283586/ListaEnlazada/List.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Text;
+
+namespace ListaEnlazada
+{
+    /// <summary>
+    /// Clase que representa una lista enlazada simple que almacena nodos de tipo Object
+    /// Esta clase contiene 4 metodos principales:
+    /// <li>AddNode()</li>
+    /// <li>RemoveNode()</li>
+    /// <li>GetNode()</li>
+    /// <li>Print()</li>
+    /// </summary>
+    public class List
+    {
+
+        Node _lead; //  Cabeza de la lista
+        int _numberOfElements; // Numero de elemtos de la lista
+
+        /// <summary>
+        /// Propiedad de campos del numero de elementos de la lista
+        /// </summary>
+        public int NumberOfElements
+        {
+            get
+            {
+                return this._numberOfElements;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de la lista que inicilizara el campo <em>_lead</em> a null y el
+        /// <em>_numberOfElements</em> a 0.
+        /// </summary>
+        public List()
+        {
+            _lead = null;
+        }
+
+        /// <summary>
+        /// Metodo que comprueba si la lista esta vacia.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsListEmpty()
+        {
+            return NumberOfElements == 0;
+        }
+
+        /// <summary>
+        /// Metodo que añade un nodo al final de la lista
+        /// </summary>
+        /// <param name="node">Nodo a añadir a la lista</param>
+        public void AddNode(Object node)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("El elemento a añadir es incorrecto.");
+                return;
+            }
+            if (IsListEmpty())
+            {
+                _lead = new Node(node);
+                _numberOfElements++;
+#if DEBUG
+                Console.WriteLine("Añadiendo nodo: " + node);
+#endif
+            }
+            else
+            {
+                Node aux = _lead;
+                while (aux.NextNode != null)
+                {
+                    aux = aux.NextNode;
+                }
+                aux.NextNode = new Node(node);
+                _numberOfElements++;
+#if DEBUG
+                Console.WriteLine("Añadiendo nodo: " + node);
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Metodo que añade un nodo en un indice pasado como parametro.
+        /// </summary>
+        /// <param name="node">Nodo a añadir a la lista</param>
+        /// <param name="index">Indice (posicion) donde añadir el nodo</param>
+        public void AddNode(Object node, uint index)
+        {
+            if (node == null)
+            {
+                Console.WriteLine("El elemento a añadir es incorrecto.");
+                return;
+            }
+            if (IsListEmpty())
+            {
+                _lead = new Node(node);
+                _numberOfElements++;
+#if DEBUG
+                Console.WriteLine("Añadiendo nodo: " + node + ", en posicion: " + 1);
+#endif
+            }
+            else if (index >= _numberOfElements)
+            {
+                Console.WriteLine("El indice indicado no es alcanzable.");
+                return;
+            }
+            else
+            {
+                if (index == 0)
+                {
+                    Node copy = _lead;
+                    _lead = new Node(node);
+                    _lead.NextNode = copy;
+                    _numberOfElements++;
+                }
+                else
+                {
+                    Node aux = _lead;
+                    for (int i = 1; i < index; i++)
+                    {
+                        aux = aux.NextNode;
+                    }
+                    Node copy = aux.NextNode;
+                    aux.NextNode = new Node(node);
+                    aux.NextNode.NextNode = copy;
+                    _numberOfElements++;
+#if DEBUG
+                    Console.WriteLine("Añadiendo nodo: " + node + ", en posicion: " + index);
+#endif
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo que elimina en nodo de la lista pasando el campo <em>_data</em> del
+        /// nodo a eliminar, de esta manera se podra encontrar el nodo en la lista y asi
+        /// poder eliminarlo.
+        /// </summary>
+        /// <param name="node">Campo <em>_data</em> del nodo a eliminar.</param>
+        public void Remove(Object node)
+        {
+            if (node == null)
+            {
+                //Console.WriteLine("El elemento a eliminar es invalido.");
+                return;
+            }
+            if (IsListEmpty())
+            {
+                return;
+            }
+
+            if (_lead.Data.Equals(node))
+            {
+                _lead = _lead.NextNode;
+                _numberOfElements--;
+                //Console.WriteLine("Eliminando el nodo cabeza de la lista.");
+                return;
+            }
+
+            Node aux = _lead;
+            while (aux.NextNode != null)
+            {
+                if (aux.NextNode.Data.Equals(node))
+                {
+                    aux.NextNode = aux.NextNode.NextNode;
+                    _numberOfElements--;
+                    //Console.WriteLine("Eliminando el nodo, " + node + " de la lista.");
+                    return;
+                }
+
+                aux = aux.NextNode;
+            }
+
+            Console.WriteLine("El elemento: " + node + ", no existe en el stack.");
+        }
+
+        /// <summary>
+        /// Metodo que devuelve el nodo que se encuentra en el indice (posicion) 
+        /// pasado como parametro.
+        /// </summary>
+        /// <param name="index">Indice (posicion) del nodo a obtener.</param>
+        /// <returns></returns>
+        public Object GetElement(uint index)
+        {
+            if (index >= _numberOfElements)
+            {
+                Console.WriteLine("La posicion no existe.");
+                return -1;
+            }
+            else
+            {
+                Node aux = _lead;
+                for (int i = 1; i <= index; i++)
+                {
+                    aux = aux.NextNode;
+                }
+                Object res = aux.Data;
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// Metodo que comprueba si un elemento de la lista pasado como parametro esta
+        /// en la lista.
+        /// </summary>
+        /// <param name="node">Elemento a comprobar si existe en la lista.</param>
+        /// <returns>true o false, dependiendo de si esta en la lista o no respectivamente</returns>
+        public bool Contains(Object node)
+        {
+            Node aux = _lead;
+            for (int i = 1; i <= NumberOfElements; i++)
+            {
+                if (aux.Data.Equals(node))
+                {
+                    return true;
+                }
+                aux = aux.NextNode;
+
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Metodo que imprime la lista con el siguiente formato:
+        /// _data - _data - _data
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        /// <summary>
+        /// Metodo ToString que devuelve una cadena con la informacion almacenada en la lista.
+        /// Se utiliza el StringBuilder para crear dicha cadena.
+        /// </summary>
+        /// <returns>Cadena con la informacion de la lista.</returns>
+        public override String ToString()
+        {
+            if (IsListEmpty())
+            {
+                Console.WriteLine("Lista vacia.");
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            Node actual = _lead;
+
+            sb.Insert(0, actual.Data);
+
+            while (actual.NextNode != null)
+            {
+                sb.AppendFormat(" - {0}", actual.NextNode.Data.ToString());
+                actual = actual.NextNode;
+            }
+
+            Console.WriteLine("\n");
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Practica3 - UO283586/stack/Stack.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+using ListaEnlazada;
+
+namespace TPP.Laboratory.ObjectOrientation.Lab03 {
+
+    public class Stack {
+
+        List lista;
+        uint _maxNumberOfElements;
+
+        public static void Main() {
+
+            Stack stack = new Stack(5);
+            try
+            {
+                stack.Push(3);
+                stack.Push(7);
+                stack.Push(6);
+                stack.Push(1);
+                //stack.Push(null);
+                stack.Push(9);
+                Console.WriteLine($"Stack: {stack}\n");
+                stack.Pop();
+                stack.Push(2);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex2)
+            {
+                Console.WriteLine(ex2.Message);
+            }
+
+        }
+
+        public Stack(uint maxNumberOfElements)
+        {
+            if (maxNumberOfElements == 0)
+            {
+                throw new ArgumentException("Numero maximo de elementos ilogico.");
+            }
+            lista = new List();
+            this._maxNumberOfElements = maxNumberOfElements;
+            Debug.Assert(this.IsEmpty);
+        }
+
+        public void Push(Object elemento)
+        {
+            Invariant();
+            if (this.IsFull)
+            {
+                throw new InvalidOperationException("El stack ya esta lleno.");
+            }
+            if (elemento == null)
+            {
+                throw new ArgumentException("El elemento no puede ser null.");
+            }
+            int previousStackLength = lista.NumberOfElements;
+
+            this.lista.AddNode(elemento);
+
+            Debug.Assert(!this.IsEmpty);
+            Debug.Assert(this.lista.NumberOfElements == (previousStackLength + 1));
+            Invariant();
+        }
+
+
+        public Object Pop()
+        {
+            if (this.IsEmpty)
+            {
+                throw new InvalidOperationException("No hay nada en el stack (imposible hacer pop)");
+            }
+
+            Object res = this.lista.GetElement(0);
+            this.lista.Remove(res);
+
+            Debug.Assert(!this.IsFull);
+            Debug.Assert(res != null);
+
+            return res;
+        }
+
+        public int GetSize()
+        {
+            return this.lista.NumberOfElements;
+        }
+
+
+        private void Invariant()
+        {
+            Debug.Assert(this.GetSize() <= _maxNumberOfElements);
+        }
+
+        public override string ToString()
+        {
+            return this.lista.ToString();
+        }
+
+        public bool IsEmpty
+        {
+            get 
+            {
+                return lista.NumberOfElements == 0;
+            }
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return lista.NumberOfElements == _maxNumberOfElements;
+            }
+        }
+
+        /**
+         * Una invariante seria que la lista no puede estar vacia y llena a la vez
+         * 
+         * En el caso del constructor del stack una precondicion seria que el numerodeelementos no sea igual aa 0
+         * En el caso del constructor del stack una postcondicion seria que la propiedad IsEmpty devuelva true
+         * 
+         * En el caso del push una precondicion seria que el elemento sea valido y el stack no este llena
+         *      - En caso de que alguna precondicion se diese se deberia de lanzar una excepcion
+         * En el caso del push una postcondicion seria que no este vacia
+         *  
+         * En el caso del pop una precondicion seria mirar el estado del stack  (lista no vacia)
+         * En el caso del push una postcondicion seria que la lista no siga llena y que el elemento que obtienes no sea null
+         * 
+         * En el caso de las propiedades no se hacen precondiciones ni postcondiciones, ya que no hay seters
+         * 
+         * -------------------
+         * EJERCICIO EXTRA   -
+         * -------------------
+         * Pillar el codigo de algorithms y poner un breakpoint viendo que hace.
+         * 
+         * 
+         */
+    }
+
+}
