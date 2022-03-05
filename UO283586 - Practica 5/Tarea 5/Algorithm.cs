af972dfb5052c28abd7573672cd94d60123464b4
Jonathan Arias Busto <UO283586@uniovi.es>
2022-03-06 00:17:58 +0100

    Practica 5 (sin acabar)

---
+++ UO283586 - Practica 5/Tarea 5/Algorithm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPP.Laboratory.Functional.Lab05
+{
+    public class Algorithm
+    {
+
+        public static bool BuscarLetra(Person persona) 
+        {
+            string ultimaLetra = persona.IDNumber.Substring(persona.IDNumber.Length - 1, 1);
+
+            if (ultimaLetra.Equals("R"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool BuscarAngulo(Angle angle)
+        {
+            if (angle.Quadrant.Equals(1) && angle.Degrees.Equals(90))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static T Buscar<T>(IEnumerable<T> list, Func<T,bool> function)
+        {
+            foreach (var a in list)
+            {
+                if (function(a))
+                {
+                    return a;
+                }
+            }
+            return default(T);
+        }
+
+
+        // Mismo metodo pero con un Predicate en vez de con una Func
+
+        public static T BuscarPred<T>(IEnumerable<T> list, Predicate<T> function)
+        {
+            foreach (var a in list)
+            {
+                if (function(a))
+                {
+                    return a;
+                }
+            }
+            return default(T);
+        }
+
+
+        // METODO FILTER
+
+        public static IEnumerable<T> Filtrar<T>(IEnumerable<T> list, Func<T, bool> function)
+        {
+            IList<T> aux = new List<T>();
+
+            foreach (var a in list)
+            {
+                if (function(a))
+                {
+                    aux.Add(a);
+                }
+            }
+            return aux;
+        }
+
+        // Forma con el Predicate
+        public static IEnumerable<T> Filtrar<T>(IEnumerable<T> list, Predicate<T> function)
+        {
+            IList<T> aux = new List<T>();
+
+            foreach (var a in list)
+            {
+                if (function(a))
+                {
+                    aux.Add(a);
+                }
+            }
+            return aux;
+        }
+
+    }
+}
--- UO283586 - Practica 5/TestTarea5/TareaTest.cs
+++ UO283586 - Practica 5/TestTarea5/TareaTest.cs
@@ -1,8 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using TPP.Laboratory.Functional.Lab05;
 
-namespace TestTarea5
+namespace TPP.Laboratory.Functional.Lab05
 {
     [TestClass]
     public class TareaTest
@@ -21,6 +20,7 @@ namespace TestTarea5
         [TestMethod]
         public void FindPeopleTest()
         {
+            // Test con Find() de C#
             Person person = Array.Find(people, x=>x.FirstName.Equals("Pepe") && x.IDNumber.EndsWith('R'));
             Assert.AreEqual(person.FirstName, "Pepe");
             Assert.AreEqual(person.Surname, "Hevia");
@@ -30,25 +30,60 @@ namespace TestTarea5
             Assert.AreEqual(person.FirstName, "María");
             Assert.AreEqual(person.Surname, "Díaz");
             Assert.AreEqual(person.IDNumber, "9876384A");
+
+            // Test con funcion lambda
+            person = Algorithm.Buscar(people, x => x.FirstName.Equals("Pepe") && x.IDNumber.EndsWith('R'));
+            Assert.AreEqual(person.FirstName, "Pepe");
+            Assert.AreEqual(person.Surname, "Hevia");
+            Assert.AreEqual(person.IDNumber, "23476293R");
+
+            // Test con Func<>
+            Func<Person,bool> find = Algorithm.BuscarLetra;
+
+            person = Algorithm.Buscar(people, find);
+            Assert.AreEqual(person.FirstName, "Pepe");
+            Assert.AreEqual(person.Surname, "Hevia");
+            Assert.AreEqual(person.IDNumber, "23476293R");
+
+
+            // Test con el Predicate<>
+            Predicate<Person> buscar = Algorithm.BuscarLetra;
+            person = Algorithm.BuscarPred(people, buscar);
+            Assert.AreEqual(person.FirstName, "Pepe");
+            Assert.AreEqual(person.Surname, "Hevia");
+            Assert.AreEqual(person.IDNumber, "23476293R");
         }
 
 
         [TestMethod]
         public void FindStraightAngleTest()
         {
-            Angle angle = Array.Find(angles, x=>x.Quadrant.Equals(1) && x.Degrees == 90);
+            // Test con Find() de C#
+            Angle angle = Array.Find(angles, x => x.Quadrant.Equals(1) && x.Degrees == 90);
             Assert.AreEqual(90, angle.Degrees);
             Assert.AreEqual(1, angle.Quadrant);
 
             angle = Array.Find(angles, x => x.Quadrant.Equals(3) && x.Degrees == 220);
             Assert.AreEqual(220, angle.Degrees);
             Assert.AreEqual(3, angle.Quadrant);
+
+            // Test con Func<>
+            Func<Angle, bool> find = Algorithm.BuscarAngulo;
+            angle = Algorithm.Buscar(angles, find);
+            Assert.AreEqual(90, angle.Degrees);
+            Assert.AreEqual(1, angle.Quadrant);
+
+            // Test con Predicate
+            Predicate<Angle> buscar = Algorithm.BuscarAngulo;
+            angle = Algorithm.BuscarPred(angles, buscar);
+            Assert.AreEqual(90, angle.Degrees);
+            Assert.AreEqual(1, angle.Quadrant);
         }
 
 
         [TestMethod]
         public void FilterPeopleTest()
-        {
+        { 
             
         }
     }
