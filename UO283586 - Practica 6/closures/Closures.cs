1e87f9a34f85d5493cae9c2ed4ee93e7404521a0
Jonathan Arias Busto <UO283586@uniovi.es>
2022-03-07 19:40:08 +0100

    Practica 6 (clase)

--- UO283586 - Practica 6/closures/Closures.cs
+++ UO283586 - Practica 6/closures/Closures.cs
@@ -10,22 +10,84 @@ namespace TPP.Laboratory.Functional.Lab06 {
         /// <summary>
         /// Version with a single method
         /// </summary>
-        static Func<int> Counter() {
+        static Func<int> Counter1() {
             int counter = 0;
             return () => ++counter;
         }
 
+        static void Counter2(int initValue, out Func<int> increment, out Func<int> decrement, out Action<int> assign)
+        {
+            int counter = initValue;
+            increment = () => counter++;
+            decrement = () => counter--;
+            assign = (value) => counter = value;
+        }
+
+        internal class Counter 
+        {
+            internal Func<int> increment { get; set; }
+            internal Func<int> decrement { get; set; }
+            internal Action<int> assign { get; set; }
+        }
+
+        static Counter Counter3(int initValue)
+        {
+            int counter = initValue;
+            return new Counter
+            {
+                increment = () => counter++,
+                decrement = () => counter--,
+                assign = (value) => counter = value
+            };
+        }
+
+        static (Func<int> increment, Func<int> decrement, Action<int> assign) Counter4(int initValue) 
+        {
+            int counter = initValue;
+            return (
+                () => counter++,
+                () => counter--,
+                (value) => counter = value
+            );
+        }
+
         static void Main() {
-            Func<int> counter = Counter();
+            Func<int> counter = Counter1();
             Console.WriteLine(counter());
             Console.WriteLine(counter());
             
-            Func<int> anotherCounter = Counter();
+            Func<int> anotherCounter = Counter1();
             Console.WriteLine(anotherCounter());
             Console.WriteLine(anotherCounter());
 
             Console.WriteLine(counter());
             Console.WriteLine(counter());
+            Console.WriteLine();
+
+            Counter2(10, out Func<int> increment, out Func<int> decrement, out Action<int> assign);
+            Console.WriteLine(increment()); // Primero se devuelve y luego se incrementa
+            Console.WriteLine(increment()); // Primero se devuelve y luego se incrementa
+            assign(20);
+            Console.WriteLine(decrement()); // Primero se devuelve y luego se decrementa
+            Console.WriteLine(decrement()); // Primero se devuelve y luego se decrementa
+
+            Console.WriteLine();
+
+            var c = Counter3(100);
+            Console.WriteLine(c.increment()); // Primero se devuelve y luego se incrementa
+            Console.WriteLine(c.increment()); // Primero se devuelve y luego se incrementa
+            c.assign(200);
+            Console.WriteLine(c.decrement()); // Primero se devuelve y luego se decrementa
+            Console.WriteLine(c.decrement()); // Primero se devuelve y luego se decrementa
+
+            Console.WriteLine();
+
+            var tuple = Counter4(1000);
+            Console.WriteLine(tuple.increment()); // Primero se devuelve y luego se incrementa
+            Console.WriteLine(tuple.increment()); // Primero se devuelve y luego se incrementa
+            tuple.assign(2000);
+            Console.WriteLine(tuple.decrement()); // Primero se devuelve y luego se decrementa
+            Console.WriteLine(tuple.decrement()); // Primero se devuelve y luego se decrementa
         }
     }
 
--- UO283586 - Practica 6/currying/Program.cs
+++ UO283586 - Practica 6/currying/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TPP.Laboratory.Functional.Lab06 {
 
-    class Program {
+    static class Program {
 
         static int Addition(int a, int b) {
             return a + b;
@@ -13,8 +15,74 @@ namespace TPP.Laboratory.Functional.Lab06 {
             return b => b + a;
         }
 
+        static IEnumerable<TCD> Map<TD, TCD>(this IEnumerable<TD> collection, Func<TD, TCD> f)
+        {
+            TCD[] result = new TCD[collection.Count()];
+            uint i = 0;
+            foreach (TD d in collection)
+            {
+                result[i] = f(d);
+                i++;
+            }
+            return result;
+        }
+
+        public static TDomain[] Filter<TDomain>(IEnumerable<TDomain> list, Func<TDomain, bool> function)
+        {
+            var aux = new TDomain[list.Count()];
+            int i = 0;
+
+            foreach (var a in list)
+            {
+                if (function(a))
+                {
+                    aux[i] = a;
+                    i++;
+                }
+            }
+            Array.Resize(ref aux, i);
+            return aux;
+        }
+
+        // Esta forma de trabajar se llama method chaning para poder hacer una llamada de metodo mas util,
+        // tambien se llama FluentAPIs
+        static IEnumerable<T> Show<T>(this IEnumerable<T> collection)
+        {
+            foreach (T element in collection)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+            return collection;
+        }
+
+        static Predicate<int> GreaterThan(int value)
+        {
+            return b => b > value;
+        }
+
         static void Main() {
             Console.WriteLine(Addition(1, 2));
+            Console.WriteLine(CurryedAdd(1)(2));
+            var incrementa = CurryedAdd(1);
+            Console.WriteLine(incrementa(2));
+
+            int[] values1 = new int[] { -3, -2, -1, 0, 1, 2, 3 };
+            values1.Map(x => Addition(x,1)).Show(); // version sin currificacion
+
+            values1.Map(CurryedAdd(1)).Show();
+
+            values1.Map(incrementa).Show();
+
+            var random = new Random();
+            var ints = new int[10];
+            ints.Map(x => random.Next(-100, 100)).Show();
+
+
+            // Genera 10 numeros aleatorios, le sumo 1 y lo muestro
+            ints.Map(_ => random.Next(-100, 100)).Show().Map(CurryedAdd(1)).Show().Filter(GreaterThan(0)).Show(); // Como no nos interesa el valor de x no lo usamos
+
+
         }
 
     }
