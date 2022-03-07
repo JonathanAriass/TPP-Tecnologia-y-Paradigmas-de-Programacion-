b1cca690ae01d3217f7b6e11ffda362358556fdd
Jonathan Arias Busto <UO283586@uniovi.es>
2022-03-07 20:01:32 +0100

    Practica 6 (clase)

--- UO283586 - Practica 6/generators/Fibonacci.cs
+++ UO283586 - Practica 6/generators/Fibonacci.cs
@@ -10,12 +10,22 @@ namespace TPP.Laboratory.Functional.Lab06 {
         static internal IEnumerable<int> InfiniteFibonacci() {
             int first = 1, second = 1;
             while (true) {
-                yield return first;
+                yield return first; // la ejecucion se queda parada aqui hasta que se llame al next()
                 int addition = first + second;
                 first = second;
                 second = addition;
             }
+        }// Cuando se llama al break en el foreach de Program.cs se llama al Dispose() del enumerator y se para la ejecucion
+
+
+        // skip ignora los n-valores que pasas como parametros y take toma los n-valores pasados como parametros
+        static public IEnumerable<int> LazyFibonacci(int from, int to) 
+        {
+            return InfiniteFibonacci().Skip(from - 1).Take(to - from - 1);
         }
+
+        // Hay que comparar el lazyfibonacci con el eagerfibonacci con el chrono
+        // hay que implementar el LazyFibonacci de forma eager
 
     }
 
--- UO283586 - Practica 6/generators/Program.cs
+++ UO283586 - Practica 6/generators/Program.cs
@@ -5,7 +5,7 @@ namespace TPP.Laboratory.Functional.Lab06 {
     class Program {
         static void Main() {
             int i = 0;
-            foreach (int value in Fibonacci.InfiniteFibonacci()) {
+            foreach (int value in Fibonacci.InfiniteFibonacci()) { // poner un punto de interrupcion aqui y ir dandole al F11
                 Console.Write(value + " ");
                 if (++i == 10)
                     break;
@@ -14,6 +14,15 @@ namespace TPP.Laboratory.Functional.Lab06 {
 
             i = 0;
             foreach (int value in Fibonacci.InfiniteFibonacci()) {
+                Console.Write(value + " ");
+                if (++i == 10)
+                    break;
+            }
+            Console.WriteLine();
+
+
+            foreach (int value in Fibonacci.LazyFibonacci(10,20))
+            {
                 Console.Write(value + " ");
                 if (++i == 10)
                     break;
