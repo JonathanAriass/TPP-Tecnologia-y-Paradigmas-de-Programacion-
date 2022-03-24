ef03ce514e07a2deea50c16be979fad7b7b6addd
Jonathan Arias Busto <UO283586@uniovi.es>
2022-03-24 23:27:08 +0100

    Practica 9.1 (sin acabar)

---
+++ UO283586 - Practica 9.1/codigo.alumnos/Tarea9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using masterWorker;
+
+namespace Lab09
+{
+
+    class Program
+    {
+        /*
+         * This program processes Bitcoin value information obtained from the
+         * url https://api.kraken.com/0/public/OHLC?pair=xbteur&interval=5.
+         */
+        static void Main(string[] args)
+        {
+            var data = Utils.GetBitcoinData();
+            foreach (var d in data)
+                Console.WriteLine(d);
+
+
+            var numberOfThreads = Convert.ToInt32(args[0]);
+            Master master = new Master(data, numberOfThreads);
+        }
+    }
+}
---
+++ UO283586 - Practica 9.1/codigo.alumnos/masterWorker/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace masterWorker
+{
+    internal class Worker
+    {
+
+        private short[] vector;
+
+        private int fromIndex, toIndex;
+
+        private long result;
+
+        internal long Result
+        {
+            get { return this.result; }
+        }
+
+        internal Worker(short[] vector, int fromIndex, int toIndex)
+        {
+            this.vector = vector;
+            this.fromIndex = fromIndex;
+            this.toIndex = toIndex;
+        }
+
+        internal void Compute()
+        {
+            this.result = 0;
+            for (int i = this.fromIndex; i <= this.toIndex; i++)
+                this.result += this.vector[i] * this.vector[i];
+        }
+
+    }
+}
