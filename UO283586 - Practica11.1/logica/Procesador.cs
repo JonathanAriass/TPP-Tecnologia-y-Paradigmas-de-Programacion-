0e06890e97be1e7e438d42bf42bc9e5acb31dd58
Jonathan Arias Busto <UO283586@uniovi.es>
2022-04-24 17:21:36 +0200

    Practica 11 (acabada)

---
+++ UO283586 - Practica11.1/logica/Procesador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace logica
+{
+    public class Procesador
+    {
+
+        private static void ContarPalabra(string palabra, string[] palabras) {
+            int repeticiones = palabras.Count(p => p.Equals(palabra));
+            //Console.WriteLine($"Palabra: {palabra}\t\tRepeticiones: {repeticiones}");
+        }
+
+        public static void PalabrasRepetidasSecuencial(string[] palabras) {
+            var textoFiltrado = palabras.Distinct();
+
+            foreach (var i in textoFiltrado) {
+                ContarPalabra(i, palabras);
+            }
+        }
+
+        
+
+        public static void PalabrasRepetidasParallel(string[] palabras) {
+            //var threads = new HashSet<int>();
+            var textoFiltrado = palabras.Distinct();
+            //Console.WriteLine($"Numero de lineas: {textoFiltrado.Count()}");
+            Parallel.ForEach(textoFiltrado, p => {
+                /*lock (threads) {
+                    threads.Add(Thread.CurrentThread.ManagedThreadId);
+                }*/
+                ContarPalabra(p, palabras);
+                });
+            //Console.WriteLine($"using {threads.Count} threads");
+        }
+
+        public static Dictionary<string, int> PalabrasRepetidasAsParallel(string[] palabras) {
+            return palabras.AsParallel().Aggregate(new Dictionary<string, int>(), (dict1, word) =>
+            {
+                // Se necesita el lock puesto que el diccionario puede estar seleccionando la misma palabra en varios
+                // hilos y si es la primera vez que aparece el resultado seria 1 y no 2
+                lock (dict1) { 
+                    if (!dict1.ContainsKey(word))
+                        dict1[word] = 1;
+                    else
+                        dict1[word]++;
+                    return dict1;   
+                }
+            });
+        }
+
+        public static string[] DividirPalabras(String texto) {
+            return texto.Split(new char[] { ' ', '\r', '\n', ',', '.', ';', ':', '-', '!', '¡', '¿', '?', '/', '«',
+                                            '»', '_', '(', ')', '\"', '*', '\'', 'º', '[', ']', '#' },
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static String LeerFicheroText(string fichero) {
+            using (StreamReader stream = File.OpenText(fichero)) {
+                StringBuilder text = new StringBuilder();
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    text.AppendLine(line);
+                }
+                return text.ToString();
+            }
+        }
+
+    }
+}
---
+++ UO283586 - Practica11.1/main/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using logica;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = Procesador.LeerFicheroText(@"..\..\..\..\clarin.txt");
+            string[] words = Procesador.DividirPalabras(text);
+
+            Stopwatch crono = new Stopwatch();
+            crono.Start();
+            Procesador.PalabrasRepetidasParallel(words);
+            crono.Stop();
+
+            Console.WriteLine("\nElapsed time: {0:N} milliseconds.", crono.ElapsedMilliseconds);
+
+
+            crono.Reset();
+
+
+            crono.Start();
+            Procesador.PalabrasRepetidasSecuencial(words);
+            crono.Stop();
+
+            Console.WriteLine("\nElapsed time: {0:N} milliseconds.", crono.ElapsedMilliseconds);
+
+
+            crono.Reset();
+
+
+            crono.Start();
+            var dic = Procesador.PalabrasRepetidasAsParallel(words);
+            /*foreach (var el in dic)
+            {
+                Console.WriteLine($"Palabra: {el.Key}\t\tRepeticiones: {el.Value}");
+            }*/
+            crono.Stop();
+            //Console.WriteLine($"Numero de lineas: {dic.Count}");
+            Console.WriteLine("\nElapsed time: {0:N} milliseconds.", crono.ElapsedMilliseconds);
+        }
+    }
+}
